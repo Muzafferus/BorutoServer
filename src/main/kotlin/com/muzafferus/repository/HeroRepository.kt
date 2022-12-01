ac42ebf64998537fd79725a79c9e6c631a2eb41b
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-01 08:30:38 +0400

    Create our repository

---
+++ src/main/kotlin/com/muzafferus/repository/HeroRepository.kt
@@ -0,0 +1,18 @@
+package com.muzafferus.repository
+
+import com.muzafferus.models.ApiResponse
+import com.muzafferus.models.Hero
+
+interface HeroRepository {
+
+    val heroes: Map<Int, List<Hero>>
+
+    val page1: List<Hero>
+    val page2: List<Hero>
+    val page3: List<Hero>
+    val page4: List<Hero>
+    val page5: List<Hero>
+
+    suspend fun getAllHeroes(page: Int = 1): ApiResponse
+    suspend fun searchHeroes(name: String): ApiResponse
+}
