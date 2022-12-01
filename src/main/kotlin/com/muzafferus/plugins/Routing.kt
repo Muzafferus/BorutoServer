76bd3b4a4decc59145133c2e950c4b98a7986c46
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-01 09:01:53 +0400

    Add All Heroes information and images

--- src/main/kotlin/com/muzafferus/plugins/Routing.kt
+++ src/main/kotlin/com/muzafferus/plugins/Routing.kt
@@ -3,11 +3,16 @@ package com.muzafferus.plugins
 import com.muzafferus.routes.getAllHeroes
 import com.muzafferus.routes.root
 import io.ktor.application.*
+import io.ktor.http.content.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+
+        static("/images") {
+            resources("images")
+        }
     }
 }
