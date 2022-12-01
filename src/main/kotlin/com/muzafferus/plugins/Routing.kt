d93b785a1b0571738554ee315ac744ff3d08eb8b
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-01 08:36:13 +0400

    Add first welcome root

--- src/main/kotlin/com/muzafferus/plugins/Routing.kt
+++ src/main/kotlin/com/muzafferus/plugins/Routing.kt
@@ -1,14 +1,11 @@
 package com.muzafferus.plugins
 
+import com.muzafferus.repository.root
 import io.ktor.application.*
-import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
-
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        root()
     }
 }
