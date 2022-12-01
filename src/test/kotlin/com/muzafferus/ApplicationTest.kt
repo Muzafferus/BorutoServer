c7dd65f0f7632aa7b40223ab67d0115ea75a71be
muzeffer.pasazade <muzeffer.pasazade@1tech.co>
2022-12-01 22:19:03 +0400

    Write test for AllHeroes get hero by page fun and calculatePage fun

--- src/test/kotlin/com/muzafferus/ApplicationTest.kt
+++ src/test/kotlin/com/muzafferus/ApplicationTest.kt
@@ -1,5 +1,9 @@
 package com.muzafferus
 
+import com.muzafferus.models.ApiResponse
+import com.muzafferus.repository.HeroRepository
+import com.muzafferus.repository.NEXT_PAGE_KEY
+import com.muzafferus.repository.PREVIOUS_PAGE_KEY
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
@@ -12,9 +16,10 @@ import kotlin.test.assertEquals
 
 class ApplicationTest {
 
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
 
     @Test
-    fun testRoot() {
+    fun `access root endpoint, assert correct information`() {
         withTestApplication(moduleFunction = Application::module) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(
@@ -29,5 +34,158 @@ class ApplicationTest {
         }
     }
 
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint page default, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = true,
+                    message = "OK",
+                    prevPage = null,
+                    nextPage = 2,
+                    heroes = heroRepository.page1
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint, query all pages, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            val pages = 1..5
+            val heroes = listOf(
+                heroRepository.page1,
+                heroRepository.page2,
+                heroRepository.page3,
+                heroRepository.page4,
+                heroRepository.page5
+            )
+            pages.forEach { page ->
+                handleRequest(HttpMethod.Get, "/boruto/heroes?page=$page").apply {
+                    assertEquals(
+                        expected = HttpStatusCode.OK,
+                        actual = response.status()
+                    )
+                    val expected = ApiResponse(
+                        success = true,
+                        message = "OK",
+                        prevPage = calculatePage(page = page)[PREVIOUS_PAGE_KEY],
+                        nextPage = calculatePage(page = page)[NEXT_PAGE_KEY],
+                        heroes = heroes[page - 1]
+                    )
+                    val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+
+                    assertEquals(
+                        expected = expected,
+                        actual = actual
+                    )
+                }
+            }
+
+        }
+    }
+
+
+    private fun calculatePage(page: Int): Map<String, Int?> {
+        var prevPage: Int? = page
+        var nextPage: Int? = page
+
+        if (page in 1..4) {
+            nextPage = nextPage?.plus(1)
+        }
+
+        if (page == 5) {
+            nextPage = null
+        }
+
+        if (page in 2..5) {
+            prevPage = prevPage?.minus(1)
+        }
+
+        if (page == 1) {
+            prevPage = null
+        }
+
+        return mapOf(PREVIOUS_PAGE_KEY to prevPage, NEXT_PAGE_KEY to nextPage)
+    }
+
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint with error NumberFormatException, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes?page=aa").apply {
+                assertEquals(
+                    expected = HttpStatusCode.BadRequest,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = false,
+                    message = "Only Numbers Allowed."
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint with error IllegalArgumentException, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes?page=0").apply {
+                assertEquals(
+                    expected = HttpStatusCode.NotFound,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = false,
+                    message = "Heroes not Found."
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `access calculatePage method, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            val pages = 1..5
+            pages.forEach { page ->
+                val expected = mapOf(
+                    PREVIOUS_PAGE_KEY to if (page == 1) null else page - 1,
+                    NEXT_PAGE_KEY to if (page == 5) null else page + 1
+                )
+                val actual = heroRepository.calculatePage(page)
+
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
 
 }
